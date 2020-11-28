a190c0ce7b8a0f965664240f10c20b79a53b4f30
Cesar Romero Arroyo <cesar.romero@shopperkit.com>
2020-11-28 18:51:50 -0500

    mider

---
+++ src/app/components/configuracion/midera/midera.component.ts
@@ -0,0 +1,43 @@
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { FirebaseService } from 'src/app/core/services/firebase.service';
+import Swal from 'sweetalert2';
+
+@Component({
+  selector: 'app-midera',
+  templateUrl: './midera.component.html',
+  styleUrls: ['./midera.component.scss'],
+})
+export class MideraComponent implements OnInit , OnDestroy {
+  user: any;
+  numRUT = 0;
+  files = [];
+  selectNiveles = [];
+  nivelArriendo = [{ name:'Especializado'},{name:'Profesional'},{name:'Tecnologo'},{name:'Tecnico'},{name:'Independiente'}];
+  levelsSelected;
+  constructor(
+    private firebase: FirebaseService
+  ) { }
+
+  ngOnInit() {
+    this.user = JSON.parse(localStorage.getItem('NEGOCIAPP_USER'));
+    console.log(this.user.midera);
+  }
+
+  ngOnDestroy() {
+
+  }
+
+  next() {
+    this.user.midera.rut = this.files["files"];
+    this.user.midera.niveles = this.levelsSelected;
+    console.log(this.user.midera);
+    localStorage.setItem('NEGOCIAPP_USER', JSON.stringify(this.user));
+    this.firebase.actualizarDatos('usuario-app', this.user, this.user.id);
+    Swal.fire(
+			'',
+			'El Mider se creo correctamente',
+			'success'
+		);
+  }
+
+}
