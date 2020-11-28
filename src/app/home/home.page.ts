afb8037df107ede6e9a805e07aa2301e2c2bd6cd
Cesar Romero Arroyo <cesar.romero@shopperkit.com>
2020-11-28 13:20:44 -0500

    Update home.page.ts

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import Swal from 'sweetalert2';
 import { Plugins } from '@capacitor/core';
 import { Router } from '@angular/router';
@@ -9,7 +9,7 @@ const { Geolocation, Device } = Plugins;
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage  implements OnInit{
   services = false;
   registerData: any = {};
   images = [];
@@ -20,5 +20,39 @@ export class HomePage {
   flag: boolean = false;
   tempo: any;
   constructor() {}
+
+  ngOnInit() {
+    this.registerData['prestador']=true;
+    this.registerData['service']=true;
+    this.registerData['rent']=true;
+    this.registerData['shop']=true;
+  }
+
+  async obtenerCoordenadas(){
+    const coordinates = await Geolocation.getCurrentPosition();
+    this.registerData['lat']= coordinates.coords.latitude;
+    this.registerData['lng']= coordinates.coords.longitude;
+  }
+
+  next(){
+    if (!this.registerData.tip_ide || !this.registerData.num_ide ||
+      !this.registerData.name || !this.registerData.email  || !this.registerData.contact || !this.registerData.city) {
+      Swal.fire(
+        '',
+        'Faltan datos, por favor incluye toda tu información',
+        'error'
+      )
+      return;
+    }
+    var cc=0;
+    const info = await Device.getInfo();
+    this.registerData['uniqueid'] = info.uuid;
+    this.registerData['onesignal'] = JSON.parse(localStorage.getItem('NEGOCIAPP_ONESIGNALUI'));
+    this.registerData['active'] = true;
+    this.registerData['nameToSearch'] = this.registerData['name'].toLowerCase();
+    this.registerData['miders'] = {estado: false, categorias: [], niveles:[], rut:''};
+    this.registerData['midera'] = {estado: false, categorias: [], niveles:[], rut:''};
+    this.registerData['miderv'] = {estado: false, categorias: [], niveles:[], rut:''};}
+  }
 
 }
